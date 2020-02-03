0d3345202e15cdc188150ebb7ee1ff163435bfd8
lirui-ML <1342058369@qq.com>
2020-02-03 14:25:56 +0800

    # add problem 24

---
+++ Algorithms/24_Swap_Nodes_in_Pairs/Swap_Nodes_in_Pairs.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+描述：两两交换链表中的节点，难度（medium）
+给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+
+你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+示例:
+
+给定 1->2->3->4, 你应该返回 2->1->4->3.
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/swap-nodes-in-pairs
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+"""
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None:
+            return head
+        dummy = p = ListNode(0)
+        dummy.next = head
+        prev_node = dummy
+        while head and head.next:
+            first_node = head
+            second_node = head.next
+
+            prev_node.next = second_node
+            first_node.next = second_node.next
+            second_node.next = first_node
+
+            prev_node = first_node
+            head = first_node.next
+        return dummy.next
+
+    def swapPairs_2(self, head: ListNode) -> ListNode:
+        dummy = p = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            tmp = head.next
+            head.next = tmp.next
+            tmp.next = head
+            p.next = tmp
+            head = head.next
+            p = tmp.next
+        return dummy.next
