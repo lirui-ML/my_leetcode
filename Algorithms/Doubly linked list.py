ce9f3889f48c781a179bbcfea1f7e26b6105a634
lirui-ML <1342058369@qq.com>
2020-03-16 17:38:52 +0800

    #add leetcode question 3

---
+++ Algorithms/Doubly linked list.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Node(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+
+
+class Doubly_Linked_List:
+
+    def __init__(self):
+        self.pred = None
+        self.trim = None
+
+
+    def add(self, new_node):
+        """增加一个节点
+        :param new_node:
+        :return:
+        """
+        pass
+
+    def addfirst(self, new_node):
+        """在头部增加一个节点
+        :param new_node:
+        :return:
+        """
+
+    def delete(self, pos):
+
+
+    def removelast(self):
+
+
